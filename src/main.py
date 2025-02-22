66653ecf87b1cb43d9740953f5cd343d0ce9282b
DanyaKramer <tipakrytoi007@gmail.com>
2025-02-22 22:58:35 +0300

    added check replasement

--- src/main.py
+++ src/main.py
@@ -3,46 +3,56 @@ from bs4 import BeautifulSoup
 import telebot
 import config as config
 from telebot import types
-
+from datetime import time, datetime
+from apscheduler.schedulers.background import BackgroundScheduler
 
 bot = telebot.TeleBot(config.token) # в файле config.py содержится API ключ для работы бота
+scheduler = BackgroundScheduler()
 
-
-@bot.message_handler(commands=['start']) # приветственное сообщение
+# распознавание стикеров
+@bot.message_handler(content_types='sticker')
+def stikers_message(message):
+                   
+     sticker_id = message.sticker.file_id
+     bot.reply_to(message, f"ID стикера: {sticker_id}")
+        
+# приветственное сообщение
+@bot.message_handler(commands=['start'])
 def start(message):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
     btn1 = types.KeyboardButton("Изменения в расписании")
     markup.add(btn1)
     bot.send_message(message.chat.id, text="Привет, {0.first_name}! Я бот который покажет тебе расписание и измениения в нём.".format(message.from_user), reply_markup=markup)
 
-@bot.message_handler(content_types=['text', 'document', 'audio', 'sticker']) # контент который бот понимает 
-def get_text_messages(message):
-        
-        if message.content_type == 'sticker': #выводит ID стикера telegram
-            sticker_id = message.sticker.file_id
-            bot.reply_to(message, f"ID стикера: {sticker_id}")
-        
-        if message.text == "сосал?": #пасхалко
-            markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-            btnYes = types.KeyboardButton("Да!")
-            btnNo = types.KeyboardButton("ДА!")
-            back = types.KeyboardButton("Обратно к расписанию")
-            markup.add(btnYes, btnNo, back)
-            bot.send_message(message.from_user.id, "А ты?", reply_markup=markup)
-        elif message.text == "Обратно к расписанию":
-            start(message)
-        elif message.text == "ДА!" or message.text == "Да!": 
-            bot.send_sticker(message.from_user.id, sf_sticker_id)
-       
+@bot.message_handler(content_types='text') # контент который бот понимает 
+def get_replacements_message(message):
+
         if message.text == "Изменения в расписании": # просмотр замен 
             try:
-               bot.send_message(message.from_user.id, f"{parsing_dates()}\n" + f"{parsing_lessons()}")
+               bot.send_message(message.from_user.id, f"{parsing_dates()}\n" + f"{get_replacements()}")
             except telebot.apihelper.ApiTelegramException as e: # если замен нету, то сообщение будет пустым и в результает выскочит ошибка, чтобы этого избежать используется данная строка
                 if "message text is empty" in str(e):
                     bot.send_message(message.from_user.id, "Возможно замен нет")
 
+@bot.message_handler(content_types='text')
+def easterEgg(message):
 
-def parsing_lessons():
+    if message.text == "сосал?": #пасхалко
+            markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+            btnYes = types.KeyboardButton("Да")
+            btnNo = types.KeyboardButton("Нет")
+            back = types.KeyboardButton("Обратно к расписанию")
+            markup.add(btnYes, btnNo, back)
+            bot.send_message(message.from_user.id, "А ты?", reply_markup=markup)
+    elif message.text == "Обратно к расписанию":
+            start(message)
+    elif message.text == "Да": 
+            bot.send_sticker(message.from_user.id, sf_sticker_id)
+    elif message.text == "Нет":
+         bot.reply_to(message, "Ну нет так нет")
+
+
+def get_replacements():
         
     req = requests.get('https://mpt.ru/izmeneniya-v-raspisanii/').text #создание запроса на получение html страницы
     soup = BeautifulSoup(req, 'lxml') # парсинг страницы
@@ -71,22 +81,33 @@ def parsing_lessons():
                     ) 
                     result.append(formatted_message) # форматированное сообщение добавляется в список
     if result: # проверка списка на наличие предметов в нём
-        return "\n".join(result) # если  в списке имеются предметы, тогда функция возвращает result подсоединяя каждый предмет с новой строки
+        print("\n".join(result))  # если  в списке имеются предметы, тогда функция возвращает result подсоединяя каждый предмет с новой строки
     else: # в других случаях возвращает сообщение об отсутствии замен 
         return "Возможно замен нет"
         
 def parsing_dates():
+
     req = requests.get('https://mpt.ru/izmeneniya-v-raspisanii/').text #создание запроса на получение html страницы
-    
     soup = BeautifulSoup(req, 'lxml') # парсинг страницы
     date = soup.find('h4').text #поиск даты для дальнейшей обработки
     return date
-        
-        
+
+@bot.message_handler(content_types='text')
+def check_replasements(message):
+     get_replacements()
+     if get_replacements() != "Возможно замен нет":
+          bot.send_message(message.chat.id, get_replacements)
+     elif get_replacements() == "Возможно замен нет":
+        pass
+
+scheduler.add_job(check_replasements, 'cron', hour=15, minute=20)
+scheduler.add_job(check_replasements, 'cron', hour=17, minute=20)
+scheduler.add_job(check_replasements, 'cron', hour=19, minute=20)
+scheduler.start()
+
 if __name__ == "__main__":
 
-    # check_lessons = time(18, 0, 0) - в дальнейшем должно быть время для проверки замен 
+    chat_id = 6007204044
     sf_sticker_id = 'CAACAgIAAxkBAAIBXWe5tajRZf78MwYVP5P8stp12RZvAALJVwACoWHpS6EShjjI1IcoNgQ' # это просто id стикера для работы пасхалки
-    
     bot.polling(none_stop=True, interval=0) # поддержка работоспособности скрипта
-
+    
